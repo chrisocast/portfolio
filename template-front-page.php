4f20cdf4137f294d85dbf64142eff98ea2b4e1b7
Chris Cast <chrisocast@gmail.com>
2014-06-16 21:57:48 -0700

    Add a template for the front page

---
+++ template-front-page.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Front Page
+*/
+?>
+<?php get_header(); ?>
+	<br class="clear" />
+    <div class="row fitvids">
+    	<div class="sixteen columns">
+		<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+                <?php the_content(); ?>
+        <?php endwhile; ?>
+		</div>
+	</div>
+<?php get_footer(); ?>
+	
