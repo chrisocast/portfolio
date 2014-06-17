4d1fe99b684c6c5da8fa534dde8f6e5ca2159a09
Chris Cast <chrisocast@gmail.com>
2014-06-16 21:57:38 -0700

    Add headers to the full-width template

--- template-full-width.php
+++ template-full-width.php
@@ -4,6 +4,14 @@ Template Name: Full Width
 */
 ?>
 <?php get_header(); ?>
+
+        <div class="columns navibg withall border-color">
+            <div class="sixteen columns alpha">
+                <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+                    <h3><?php the_title(); ?></h3>
+                <?php endwhile; ?>
+            </div>
+        </div>
     
     	
     	<br class="clear" />
