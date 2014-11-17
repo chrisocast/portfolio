1c8c4801d93088ace5761223c40104c538fadaf7
Chris Cast <chrisocast@gmail.com>
2014-11-17 13:45:06 -0800

    Update some css and tweak single page template

--- works-single.php
+++ works-single.php
@@ -10,30 +10,28 @@
 	// AJAX TOKEN
 	$token = wp_create_nonce("wp_token");
 	
-	
 	$prev_post = getNextBack('prev','works',$post->menu_order,$post->post_date);
 	$next_post = getNextBack('next','works',$post->menu_order,$post->post_date);
 
-		?>	
+?>	
         
 <div id="singlecontent">
 
 		<div class="columns navibg border-color">
-            <div class="four columns alpha">
-            	<h3><?php _e('Works','dronetv'); ?></h3>
+            <div class="seven columns alpha works-title">
+            	<h3><?php _e('Works','dronetv'); ?> ></h3> <h1><?php echo get_the_title( $ID ); ?></h1>
             </div>
             
-            <div class="twelve columns omega">
+            <div class="nine columns omega">
             	<div class="navigate">
                 	<hr class="resshow border-color" /> 
                     <span class="pname"></span>
-                    <a href="<?php echo get_home_url(); ?>" data-title="All" title="<?php echo __("All Projects","dronetv");?>" data-type="works" data-token="<?php echo $token?>" class="navigate parent getworks-showmsg gohome">&nbsp;</a>
                     <?php if(!empty( $prev_post['ID'] )) { ?>
-                    <a href="<?php echo get_permalink($prev_post['ID']); ?>" data-type="works" data-token="<?php echo $token?>" data-id="<?php echo $prev_post['ID']?>" title="<?php echo htmlspecialchars($prev_post['post_title'])?>" class="navigate back getworks-nextback getworks-showmsg">&nbsp;</a>
+                      <a href="<?php echo get_permalink($prev_post['ID']); ?>" data-type="works" data-token="<?php echo $token?>" data-id="<?php echo $prev_post['ID']?>" title="Previous work: <?php echo htmlspecialchars($prev_post['post_title'])?>" class="navigate back getworks-nextback getworks-showmsg">&nbsp;</a>
                     <?php } ?>
                     <?php if(!empty( $next_post['ID'] )) { ?>
-                    <a href="<?php echo get_permalink($next_post['ID']); ?>" data-type="works" data-token="<?php echo $token?>" data-id="<?php echo $next_post['ID']?>" title="<?php echo htmlspecialchars($next_post['post_title'])?>" class="navigate next getworks-nextback getworks-showmsg">&nbsp;</a>
-					<?php } ?>
+                      <a href="<?php echo get_permalink($next_post['ID']); ?>" data-type="works" data-token="<?php echo $token?>" data-id="<?php echo $next_post['ID']?>" title="Next work: <?php echo htmlspecialchars($next_post['post_title'])?>" class="navigate next getworks-nextback getworks-showmsg">&nbsp;</a>
+					          <?php } ?>
             	</div>
             </div>	
         </div>
@@ -46,7 +44,7 @@
         $work_url = get_post_meta( $post->ID, 'work-url', true );
         $work_date = date('M jS, Y',strtotime(get_post_meta( $post->ID, 'work-date', true )));
         $terms = get_the_terms( $post->ID , 'works-categories', 'string' );
-		$descpos = get_post_meta( $post->ID , 'work-desc-position', true );
+		    $descpos = get_post_meta( $post->ID , 'work-desc-position', true );
 		 
         
         if ( $terms && ! is_wp_error( $terms ) ) {
@@ -59,45 +57,7 @@
         
     ?>      
        <div class="postwraps sixteen columns showajaxcontent border-color">
-                      <div class="fifteensp columns offset-by-half alpha">
-                           <h2 class="titles" style="text-shadow:none!important;"><a href="<?php echo $permalink ?>" style="text-shadow:none!important;"><?php the_title(); ?></a></h2>
-                           <hr />
-                      </div>  
-                      <div class="fifteensp columns offset-by-half alpha pinfo">
-                           <div class="four columns alpha">
-                           		<?php if($categories!="") {?>
-                                <strong><?php _e('Creative Fields','dronetv')?></strong> <br />
-                                <?php echo $categories; ?>  
-                                <?php } ?>
-                          </div> 
-                          <div class="four columns">
-                           		<?php if($work_client!="") {?>
-                                <strong><?php _e('Client','dronetv')?></strong> <br />
-                                <?php echo $work_client; ?>
-                                <?php } ?>
-                          </div> 
-                           <div class="four columns">
-                           		<?php if($work_url!="") {?>
-                                <strong><?php _e('Project URL','dronetv')?></strong> <br />
-                                <a href="<?php echo $work_url; ?>" target="_blank"><?php 
-								$work_url = str_replace('http://','',$work_url);
-								if(strlen($work_url) > 30) {
-								echo substr($work_url,0,30)."..."; 
-								}else{
-								echo $work_url;	
-								}
-								?></a>  
-                          		<?php } ?>
-                          </div>
-                           <div class="three columns omega">
-                           		<?php if(get_post_meta( $post->ID, 'work-date', true )) {?>
-                                <strong><?php _e('Completion Date','dronetv')?></strong><br />
-                                <?php echo $work_date; ?> 
-                                <?php } ?>  
-                          </div>
-            			  <br class="clear" />
-                          <hr />
-                      </div>
+                      
                       
                       <?php 
 					  	  $sharittop = of_get_option('md_social_post_disable_top');
@@ -163,41 +123,15 @@
 						}
 						}
 					?>
-                    <br class="clear" />
 		 	</div>     
             
-             		<?php 
-						  /// SHOW PROJECT DESCRIPTION
-						  if($descpos=='bottom') {
-							echo $workdesc;  
-							echo '<div class="fifteen columns offset-by-half alpha resdontshow"><hr></div>';
-						  }
+         	<?php 
+  				  /// SHOW PROJECT DESCRIPTION
+  				  if($descpos=='bottom') {
+  					 echo $workdesc;  
+  				  }
 					?> 
-            
-           
-                <div class="fifteensp columns offset-by-half alpha" style="margin-bottom:10px;">
-                    <div class="sharingbottom border-color bottoms"> 
-                  	<?php if(!of_get_option('md_social_post_disable_bottom')) {?>
-                    	<div class="resdontshow shr"><strong><?php _e('SHARE : ','dronetv');?></strong></div>
-                       <?php echo showshareingpost($permalink,@$pimg[0], @$ptitle,false); ?>
-					<?php } ?>
-                    &nbsp;
-                    </div>
-                        <hr class="resshow border-color-works" /> 
-                    <div class="navigate pull-right">
-                        <span class="pname"></span> 
-                    <a href="<?php echo get_home_url(); ?>" data-title="All" title="<?php echo __("All Projects","dronetv");?>" data-type="works" data-token="<?php echo $token?>" class="navigate parent getworks-showmsg gohome">&nbsp;</a>
-                    <?php if(!empty( $prev_post['ID'] )) { ?>
-                    <a href="<?php echo get_permalink($prev_post['ID']); ?>" data-type="works" data-token="<?php echo $token?>" data-id="<?php echo $prev_post['ID']?>" title="<?php echo htmlspecialchars($prev_post['post_title'])?>" class="navigate back getworks-nextback getworks-showmsg">&nbsp;</a>
-                    <?php } ?>
-                    <?php if(!empty( $next_post['ID'] )) { ?>
-                    <a href="<?php echo get_permalink($next_post['ID']); ?>" data-type="works" data-token="<?php echo $token?>" data-id="<?php echo $next_post['ID']?>" title="<?php echo htmlspecialchars($next_post['post_title'])?>" class="navigate next getworks-nextback getworks-showmsg">&nbsp;</a>
-					<?php } ?>
-                    </div>
-                </div>
-                        <br class="clear" />
-                        <br class="clear" />
-           
+          <br class="clear" /> 
 		 </div>     
   </div> 
                         
